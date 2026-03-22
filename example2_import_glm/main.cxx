8bd7d657e46dd61085fd2a0b7113ed5b98b346d1
Dominique Belhachemi <db@domibel.de>
2026-03-22 14:40:38 -0400

    simple 'import glm;' requires only c++20

--- example2_import_glm/main.cxx
+++ example2_import_glm/main.cxx
@@ -1,14 +1,9 @@
-import std;
+#include <iostream>
 import glm;
 
 int main() {
-    std::println("Testing import glm!");
-    
     glm::vec3 v1(1.0f, 2.0f, 3.0f);
     glm::vec3 v2(4.0f, 5.0f, 6.0f);
-    
-    float dot = glm::dot(v1, v2);
-    std::println("Dot product: {}", dot);
-    
-    return 0;
+
+    std::cout << "Dot product: " << glm::dot(v1, v2) << "\n";
 }
